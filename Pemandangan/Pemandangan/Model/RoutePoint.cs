3e1f9fe6764bfcef672d640faf510a7ed335a1ea
Mauro de Lyon <maurodelyon@gmail.com>
2015-12-14 12:35:23 +0100

    First structural setup

---
+++ Pemandangan/Pemandangan/Model/RoutePoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pemandangan.Model
+{
+
+    class RoutePoint
+    {
+        enum Direction
+        {
+            North,
+            NorthEast,
+            East,
+            SouthEast,
+            South,
+            SouthWest,
+            West,
+            NorthWest
+        };
+    }
+}
