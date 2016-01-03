68e0da6dd7c7432b33f6b8d796be317c03c21720
Hanburgah <arthur.brink1@gmail.com>
2016-01-03 19:01:53 +0100

    code

--- Pemandangan/Pemandangan/View/MapPage.xaml.cs
+++ Pemandangan/Pemandangan/View/MapPage.xaml.cs
@@ -4,8 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Geolocation;
+using Windows.Devices.Geolocation.Geofencing;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Services.Maps;
+using Windows.Storage;
 using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
@@ -15,6 +18,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -31,11 +35,22 @@ namespace Pemandangan.View
         private MapPolyline LatestwalkedLine;
         private List<Geopoint> walkedRoute;
 
+        private StorageFile waypoint;
+        private StorageFile seen;
+
         public MapPage()
         {
             this.InitializeComponent();
             walkedRoute = new List<Geopoint>();
+            loadAssets();
+        }
 
+        private async void loadAssets()
+        {
+            var uri1 = new System.Uri("ms-appx:///Assets/test01.jpg");
+            var uri2 = new System.Uri("ms-appx:///Assets/test01.jpg");
+            waypoint = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(uri1);
+            seen = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(uri2);
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -64,9 +79,76 @@ namespace Pemandangan.View
             currentPos.ZIndex = 4;
 
             map.MapElements.Add(currentPos);
-
+            
             await map.TrySetViewAsync(pos, 17);
 
+            setupGeofencing();
+            setupGeofences();
+        }
+
+        private void setupGeofences()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void setupGeofencing()
+        {
+            GeofenceMonitor.Current.Geofences.Clear();
+            Geolocator locator = new Geolocator();
+
+            var location = await locator.GetGeopositionAsync().AsTask();
+
+            Geofence fence
+             = GeofenceMonitor.Current.Geofences.FirstOrDefault(gf => gf.Id == "currentLoc");
+
+            if (fence == null)
+            {
+                GeofenceMonitor.Current.Geofences.Add(
+                     new Geofence("currentLoc", new Geocircle(location.Coordinate.Point.Position, 10.0), MonitoredGeofenceStates.Entered,
+                                    false, TimeSpan.FromSeconds(10))
+                        );
+            }
+            GeofenceMonitor.Current.GeofenceStateChanged += GeofenceStateChanged;
+        }
+
+        private async void GeofenceStateChanged(GeofenceMonitor sender, object args)
+        {
+            var reports = sender.ReadReports();
+
+
+
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                foreach (GeofenceStateChangeReport report in reports)
+                {
+                    GeofenceState state = report.NewState;
+                    Geofence geofence = report.Geofence;
+
+                    if (state == GeofenceState.Removed)
+                    {
+
+                    }
+
+                    else if (state == GeofenceState.Entered)
+                    {
+
+                        if (geofence.Id != "currentLoc")
+                        {
+                            var dialog = new Windows.UI.Popups.MessageDialog(geofence.Id + "Entered");
+                            var result = await dialog.ShowAsync();
+                        }
+                            
+
+
+
+                    }
+
+                    else if (state == GeofenceState.Exited)
+                    {
+
+                    }
+                }
+            });
         }
 
         private async void GeolocatorPositionChanged(Geolocator sender, PositionChangedEventArgs args)
@@ -80,6 +162,7 @@ namespace Pemandangan.View
 
             });
             walkedRoute.Add(pos);
+            drawWalkedRoute();
             await map.TrySetViewAsync(pos, 17);
         }
 
@@ -156,9 +239,28 @@ namespace Pemandangan.View
             var result = await dialog.ShowAsync();
         }
 
-        public void buildMap()
+        public async void buildMap(List<Geopoint> list)
         {
+            MapRouteFinderResult routeResult
+                = await MapRouteFinder.GetDrivingRouteFromWaypointsAsync(list);
 
+            MapRoute b = routeResult.Route;
+
+            
+            var color = Colors.Green;
+            color.A = 128;
+
+            var line = new MapPolyline
+            {
+                StrokeThickness = 11,
+                StrokeColor = color,
+                StrokeDashed = false,
+                ZIndex = 2
+            };
+
+            line.Path = new Geopath(b.Path.Positions);
+
+            map.MapElements.Add(line);
         }
 
     }
